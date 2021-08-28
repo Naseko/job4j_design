55922b0409c6b32f698c50d8056754962c77c1a2
Naseko <Naseko@users.noreply.github.com>
2021-08-28 22:52:57 +0300

    Task 39-276

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,5 +1,13 @@
 package ru.job4j;
 
+import ru.job4j.generics.Generics;
+import ru.job4j.pojo.Animal;
+import ru.job4j.pojo.Predator;
+import ru.job4j.pojo.Tiger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -7,5 +15,27 @@ package ru.job4j;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+            Generics gen = new Generics();
+            List<Animal> first = new ArrayList<>();
+            List<Predator> second = new ArrayList<>();
+            List<Tiger> third = new ArrayList<>();
+            first.add(new Animal());
+            second.add(new Predator());
+            third.add(new Tiger());
+
+            gen.printObject(first);
+            gen.printObject(second);
+            gen.printObject(third);
+            System.out.println();
+
+            gen.printBoundedWildCard(first);
+            gen.printBoundedWildCard(second);
+            gen.printBoundedWildCard(third);
+            System.out.println();
+
+            gen.printLowerBoundedWildCard(first);
+            gen.printLowerBoundedWildCard(second);
+            gen.printLowerBoundedWildCard(third);
     }
 }
---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,32 @@
+package ru.job4j.generics;
+
+import ru.job4j.pojo.Animal;
+import ru.job4j.pojo.Tiger;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+
+
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printBoundedWildCard(List<? extends Animal> list) {
+        for (Iterator<? extends Animal> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printLowerBoundedWildCard(List<? super Tiger> list) {
+        for (Iterator<? super Tiger> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
